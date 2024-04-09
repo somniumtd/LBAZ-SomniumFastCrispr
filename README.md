179e11f67072e7827ab10af7790ca7954c433879
somniumtd <23292947+somniumtd@users.noreply.github.com>
2024-04-09 19:10:57 +0200

    Initial version

---
+++ README.md
@@ -0,0 +1,4 @@
+# Somnium Fast CRISPR
+A small mod for "Let's Build a Zoo", that reduces the time to create any hybrid in the CRISPR to a single day.
+
+This mod requires the [LBAM Mod API](https://www.nexusmods.com/letsbuildazoo/mods/1)
---
+++ src/SomniumFastCrispr/SomniumFastCrisprMod.cs
@@ -0,0 +1,32 @@
+﻿using HarmonyLib;
+using ModLoader;
+using System;
+
+namespace SomniumFastCrispr
+{
+    public class SomniumFastCrisprMod : IMod
+    {
+        Harmony HarmonyInstance;
+        IModHelper Helper;
+
+        public void ModEntry(IModHelper helper)
+        {
+            Helper = helper;
+            HarmonyInstance = new Harmony("SomniumTD.SomniumFastCrispr");
+            HarmonyInstance.PatchAll();
+
+            // Using AccessTools and manual patching instead of Harmony annotations because the "CRISPRCalculator" class has visibility "internal"
+            var type = Type.GetType("TinyZoo.Z_BalanceSystems.Animals.CRISPR.CRISPRCalculator, LetsBuildAZoo");
+            HarmonyInstance.Patch(
+                    original: AccessTools.Method(type, "GetDaysForThisCRISPRBreed"),
+                    postfix: new HarmonyMethod(GetType(), nameof(CrisperOverride))
+                    );
+        }
+
+        public static void CrisperOverride(ref float __result)
+        {
+            __result = 1.0f; // Override CRISPR complexity calculation to always return a duration of 1 day
+        }
+
+    }
+}
